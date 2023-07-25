ea95acbfc5e00ce5e9f91c28151af9dbb236bc73
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-07-25 12:34:39 -0400

    Update 1.Two-Sum.py

--- 1.Two-Sum.py
+++ 1.Two-Sum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
         for index, num in enumerate(nums):
             complement = target - num
